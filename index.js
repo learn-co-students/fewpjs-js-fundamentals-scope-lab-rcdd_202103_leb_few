b1189642743736cf5c92bfca01124eeb1b17c396
Cesar Maaz <cesarmaaz@gmail.com>
2021-04-16 17:44:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-animal = ""
+
+//let animal = ""
 
 function myAnimal() {
-  return animal
+let animal = "dog"
+
+return animal
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  return animal
+let animal = "cat"
+
+// How can we make sure that this function
+// and the above function both pass?
+// P.S.: You can't just hard-code 'cat' below
+return animal
 }
 
-function add2(n) {
-  return n + two
+const n = Math.floor(Math.random() * 1000);
 
-  // Feel free to move things around!
-  two = 2
+function add2(n) {
+const two = 2;
+return n + two
+
+// Feel free to move things around!
 }
